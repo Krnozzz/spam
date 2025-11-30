c65dbae192c46acaabd9f7bca3ac73d324d80b5c
Krnozzz <krnozz.01@gmail.com>
2025-11-30 22:42:54 +0800

    update

---
+++ bomber.py
@@ -0,0 +1,29 @@
+import json
+import requests
+
+def send_sms(target_number, message):
+    api_url = "https://api.bulksms.com/v1/messages"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Basic NjIwOTY2OTkwMTZGNEU4MzgwRDcxODk0QkU0ODg2MTItMDItQzoyMUZ6KjlDQjJwcmlwU1lZSzBkaDBQMDRCNDhvYQ=="
+    }
+    data = {
+        "messages": [
+            {
+                "destination": target_number,
+                "source": "4n0s",
+                "content": "NIHGAAAAA!!"
+            }
+        ]
+    }
+    response = requests.post(api_url, headers=headers, data=json.dumps(data))
+    return response.json()
+
+def main():
+    target_number = input("Enter target phone number: ")
+    message = input("Enter message: ")
+    response = send_sms(target_number, message)
+    print(response)
+
+if __name__ == "__main__":
+    main()
